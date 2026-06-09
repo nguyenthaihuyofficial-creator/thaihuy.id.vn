6f4f67c23f93eae5050031489073c4602a473ae6
nguyenthaihuyofficial-creator <nguyenthaihuy.official@gmail.com>
2026-06-09 15:40:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+menuBtn?.addEventListener('click', () => nav.classList.toggle('open'));
+document.querySelectorAll('.nav a').forEach(link => {
+  link.addEventListener('click', () => nav.classList.remove('open'));
+});
