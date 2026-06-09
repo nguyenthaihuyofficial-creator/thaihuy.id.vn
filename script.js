fe76544d128d07e622aeb1e1ab660a0b0630227d
nguyenthaihuyofficial-creator <nguyenthaihuy.official@gmail.com>
2026-06-09 16:19:41 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
-menuBtn?.addEventListener('click', () => nav.classList.toggle('open'));
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', () => nav.classList.remove('open'));
-});
